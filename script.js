41b8571893993c2c9690a3ec3d98eba71cdd38c3
HRITIK DWIVEDI <44141619+hritikrommie@users.noreply.github.com>
2020-10-18 00:59:53 +0530

    merge

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 let keyclick = {
-    49:'#boom',
-    50:"#clap",
-    51:"#hihat",
-    52:"#kick",
-    53:"#openhat",
-    54:"#ride",
-    55:"#snare",
-    56:"#tink",
-    57:"#tom"
-
+    49: '#boom',
+    50: "#clap",
+    51: "#hihat",
+    52: "#kick",
+    53: "#openhat",
+    54: "#ride",
+    55: "#snare",
+    56: "#tink",
+    57: "#tom"
 };
-document.onkeydown=(e)=>{
+
+const cont = document.querySelector('.container');
+cont.addEventListener('click', (e) => {
+    console.log(e);
+    if (e.target.className === 'key') {
+        let id = e.target.id;
+        clickme(id);
+    }
+    else{
+        let id = e.target.parentElement.id;
+        clickme(id);
+    }
+})
+
+document.onkeydown = (e) => {
     let b = document.querySelector(keyclick[e.keyCode]);
-    if(b){
-       b.currentTime=0;
+    if (b) {
+        b.currentTime = 0;
     }
     b.play();
+    document.querySelector(keyclick[e.keyCode] + '_').classList.add('trans');
+    setTimeout(() => {
+        document.querySelector(keyclick[e.keyCode] + '_').classList.remove('trans');
+    }, 100);
     console.log(e);
 }
-function play(keycode){
-  
+function clickme(id){
+    let n = id.length;
+        n = n - 1;
+        id = id.substring(0,n);
+        console.log(id);
+        let b = document.querySelector('#'+id);
+        if (b) {
+            b.currentTime = 0;
+        }
+        b.play();
+        document.querySelector('#'+id + '_').classList.add('trans');
+        setTimeout(() => {
+            document.querySelector('#'+id + '_').classList.remove('trans');
+        }, 100);
+        console.log(n);
 }
